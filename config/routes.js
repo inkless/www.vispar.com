6f5e6564f085c26429afa2900dfad09380a046df
Zhang Guangda <zhang.gd@foxmail.com>
2015-02-17 09:27:31 -0800

    simpliest user login

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,17 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+
+  // HTML Views
+  'get /login': { view: 'user/login' },
+  'get /signup': { view: 'user/signup' },
+  '/welcome': { view: 'user/welcome' },
+
+  // Endpoints
+  'post /login': 'UserController.login',
+  'post /signup': 'UserController.signup',
+  '/logout': 'UserController.logout'
 
   /***************************************************************************
   *                                                                          *
